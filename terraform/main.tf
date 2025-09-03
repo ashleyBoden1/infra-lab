1a9a64d43a21f86eeaf610d2fd0971a152763b27
Ashley <ashley.boden@outlook.com>
2025-09-03 11:20:21 +0100

    Fresh start: add Terraform config with .gitignore

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project = "speedy-realm-470614-f8"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-vm"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {}
+  }
+}
